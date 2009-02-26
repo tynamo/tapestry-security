1b965acaf683b3a121eca8a5207c0376019c7829
Kalle Korhonen <kaosko@apache.org>
2009-02-26 04:39:45 +0000

    Remove commented out code

--- src/main/java/org/trailsframework/security/services/SecurityFilterChain.java
+++ src/main/java/org/trailsframework/security/services/SecurityFilterChain.java
@@ -1,16 +1,9 @@
 package org.trailsframework.security.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.Filter;
-
 import org.apache.tapestry5.services.HttpServletRequestHandler;
 
 public class SecurityFilterChain {
 	private String path;
-
-	private List<Filter> filters = new ArrayList<Filter>();
 
 	private HttpServletRequestHandler handler;
 
@@ -26,24 +19,4 @@ public class SecurityFilterChain {
 	protected String getPath() {
 		return path;
 	}
-
-	/*
-	protected List<Filter> getFilters() {
-		return filters;
-	}
-
-	public SecurityFilterChain add(Filter filter) {
-		return add(filter, null);
-	}
-
-	public SecurityFilterChain add(Filter filter, String filterConfig) {
-		if (filter instanceof PathMatchingFilter) ((PathMatchingFilter) filter).processPathConfig(path, filterConfig);
-		filters.add(filter);
-		return this;
-	}
-
-	public boolean isEmpty() {
-		return filters.size() == 0;
-	}
-	*/
 }
