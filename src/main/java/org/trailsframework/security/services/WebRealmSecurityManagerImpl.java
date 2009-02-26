7f153a8a4929227985149263412f0a4ed981f7eb
Kalle Korhonen <kaosko@apache.org>
2009-02-26 04:39:09 +0000

    Add comment

--- src/main/java/org/trailsframework/security/services/WebRealmSecurityManagerImpl.java
+++ src/main/java/org/trailsframework/security/services/WebRealmSecurityManagerImpl.java
@@ -5,6 +5,10 @@ import java.util.Collection;
 import org.jsecurity.realm.Realm;
 import org.jsecurity.web.DefaultWebSecurityManager;
 
+/**
+ * Needed just to point out to tapestry-ioc the right constructor to use (from the three available in
+ * DefaultWebSecurityManager)
+ */
 public class WebRealmSecurityManagerImpl extends DefaultWebSecurityManager {
 	public WebRealmSecurityManagerImpl(final Collection<Realm> realms) {
 		super(realms);
