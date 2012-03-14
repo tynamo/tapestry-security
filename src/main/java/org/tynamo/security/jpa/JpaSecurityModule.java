5eac585eb01a0483883d10f279e5ca7af29a53a6
Kalle Korhonen <kaosko@apache.org>
2012-03-14 05:54:13 +0000

    Remove unused code

--- src/main/java/org/tynamo/security/jpa/JpaSecurityModule.java
+++ src/main/java/org/tynamo/security/jpa/JpaSecurityModule.java
@@ -1,11 +1,8 @@
 package org.tynamo.security.jpa;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 
 import javax.persistence.EntityManager;
-import javax.persistence.metamodel.SingularAttribute;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.tapestry5.ioc.MethodAdviceReceiver;
@@ -36,28 +33,4 @@ public class JpaSecurityModule {
 		}
 
 	}
-
-	/**
-	 * Fetches the value of the given SingularAttribute on the given entity.
-	 * 
-	 * @see http://stackoverflow.com/questions/7077464/how-to-get-singularattribute-mapped-value-of-a-persistent-object
-	 */
-	@SuppressWarnings("unchecked")
-	public static <EntityType, FieldType> FieldType getValue(EntityType entity,
-		SingularAttribute<EntityType, FieldType> field) {
-		try {
-			Member member = field.getJavaMember();
-			if (member instanceof Method) {
-				// this should be a getter method:
-				return (FieldType) ((Method) member).invoke(entity);
-			} else if (member instanceof Field) {
-				return (FieldType) ((Field) member).get(entity);
-			} else {
-				throw new IllegalArgumentException("Unexpected java member type. Expecting method or field, found: " + member);
-			}
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 }
