8ec7f5966fce30ca3fcd4af655a16501c3158ec5
Alejandro Scandroli <ascandroli@codehaus.org>
2012-05-16 21:58:49 +0000

    tabifying

--- src/main/java/org/tynamo/security/services/TapestryRealmSecurityManager.java
+++ src/main/java/org/tynamo/security/services/TapestryRealmSecurityManager.java
@@ -1,7 +1,5 @@
 package org.tynamo.security.services;
 
-import java.util.Collection;
-
 import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.RememberMeManager;
 import org.apache.shiro.mgt.SubjectFactory;
@@ -11,21 +9,23 @@ import org.apache.shiro.web.mgt.DefaultWebSessionStorageEvaluator;
 import org.apache.shiro.web.session.mgt.ServletContainerSessionManager;
 import org.tynamo.security.Authenticator;
 
+import java.util.Collection;
+
 /**
  * This class is needed to point out the right constructor to use (from the three available in
- * DefaultWebSecurityManager) for tapestry-ioc and to allow injecting dependencies  
+ * DefaultWebSecurityManager) for tapestry-ioc and to allow injecting dependencies
  */
 public class TapestryRealmSecurityManager extends DefaultWebSecurityManager {
 
 	// Could easily make sessionStorageevaluator and sessionManager provided services as well, add as needed
 	public TapestryRealmSecurityManager(Authenticator authenticator, SubjectFactory subjectFactory, RememberMeManager rememberMeManager, final Collection<Realm> realms) {
-    super();
-    authenticator.setRealms(realms);
-    setAuthenticator(authenticator);
-    ((DefaultSubjectDAO) this.subjectDAO).setSessionStorageEvaluator(new DefaultWebSessionStorageEvaluator());
-    setSubjectFactory(subjectFactory);
-    setRememberMeManager(rememberMeManager);
-    setSessionManager(new ServletContainerSessionManager());
-    setRealms(realms);
-	}	
+		super();
+		authenticator.setRealms(realms);
+		setAuthenticator(authenticator);
+		((DefaultSubjectDAO) this.subjectDAO).setSessionStorageEvaluator(new DefaultWebSessionStorageEvaluator());
+		setSubjectFactory(subjectFactory);
+		setRememberMeManager(rememberMeManager);
+		setSessionManager(new ServletContainerSessionManager());
+		setRealms(realms);
+	}
 }
