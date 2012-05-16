daa71cea002641541ed971a890bd138fc703d6d9
Alejandro Scandroli <ascandroli@codehaus.org>
2012-05-16 22:05:38 +0000

    fixes TYNAMO-154 - added a new FirstExceptionAuthenticationStrategy and marked it as the default AuthenticationStrategy for tapestry-security.

--- src/main/java/org/tynamo/security/internal/ModularRealmAuthenticator.java
+++ src/main/java/org/tynamo/security/internal/ModularRealmAuthenticator.java
@@ -2,13 +2,20 @@ package org.tynamo.security.internal;
 
 import org.apache.shiro.authc.AuthenticationListener;
 import org.tynamo.security.Authenticator;
+import org.tynamo.shiro.extension.authc.pam.FirstExceptionStrategy;
 
 public class ModularRealmAuthenticator extends org.apache.shiro.authc.pam.ModularRealmAuthenticator implements Authenticator {
+
+	public ModularRealmAuthenticator() {
+		super();
+		setAuthenticationStrategy(new FirstExceptionStrategy());
+	}
+
 	@Override
 	public void addAuthenticationListener(AuthenticationListener authenticationListener) {
 		getAuthenticationListeners().add(authenticationListener);
 	}
-		
+
 	@Override
 	public void removeAuthenticationListener(AuthenticationListener authenticationListener) {
 		getAuthenticationListeners().remove(authenticationListener);
