336fd289af33495a5da768dbb38930b3e2d5e52a
Alejandro Scandroli <ascandroli@codehaus.org>
2012-06-08 08:08:01 +0000

    added the ElementType.METHOD target so I can use the @Security annotation without the @Marker annotation (related to TYNAMO-143)

--- src/main/java/org/tynamo/security/Security.java
+++ src/main/java/org/tynamo/security/Security.java
@@ -2,12 +2,13 @@ package org.tynamo.security;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({java.lang.annotation.ElementType.PARAMETER, java.lang.annotation.ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({PARAMETER, FIELD, METHOD})
+@Retention(RUNTIME)
 @Documented
-public @interface Security
-{
+public @interface Security {
 }
