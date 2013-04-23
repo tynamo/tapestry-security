60cd9156a9565cc38f237346a3a9fdb0aee9759b
Kalle Korhonen <kaosko@apache.org>
2013-04-23 05:15:38 +0000

    FIXED - TYNAMO-125: Else block for the security components - make all simple components implement AbstractConditional and implement() - all tests pass. Probably should write some new tests for it but nothing can go wrong, right?

--- src/main/java/org/tynamo/security/components/HasAnyPermissions.java
+++ src/main/java/org/tynamo/security/components/HasAnyPermissions.java
@@ -1,32 +1,14 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 package org.tynamo.security.components;
 
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.corelib.base.AbstractConditional;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.tynamo.security.services.SecurityService;
 
 /**
  * @see SecurityService#hasAnyPermissions(String)
  */
-public class HasAnyPermissions
-{
+public class HasAnyPermissions extends AbstractConditional {
 
 	@Inject
 	private SecurityService securityService;
@@ -37,6 +19,12 @@ public class HasAnyPermissions
 
 	boolean beforeRenderBody()
 	{
+		return test();
+	}
+
+
+	@Override
+	protected boolean test() {
 		return securityService.hasAnyPermissions(permissions);
 	}
 }
